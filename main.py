990865cde2dcf0ba65eb51c1509a976a8b016317
FLEYreal <borisov.nikita.off@gmail.com>
2023-12-19 21:25:54 +0800

    -

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 # Libs
 import json
+from colorama import init
 
 # Files
 from screenshots import get_screenshots
@@ -16,12 +17,14 @@ from screenshots import get_screenshots
 # Main function to work with
 def main():
 
+    init()
+
     # Load Config
     with open('config.json', 'r') as file:
         config = json.load(file)
 
     # Get screenshots
-    get_screenshots(config['screenshot_amount'])
+    get_screenshots(config['attempts_amount'])
 
 
 if(__name__ == '__main__'):
--- screenshots.py
+++ screenshots.py
@@ -1,11 +1,22 @@
+# '//st.prntscr.com/2023/07/24/0635/img/0_173a7b_211be8ff.png'
 
+# Basics
+import os
 import requests
 import random
 import json
+
+# Libs
 from bs4 import BeautifulSoup
+from colorama import Fore
 
 url_template = 'https://prnt.sc/'
 symbols = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+def generate_random_string(length=7):
+    letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return ''.join(random.choice(letters) for _ in range(length))
+
 
 def get_screenshots(amount: int):
 
@@ -18,34 +29,47 @@ def get_screenshots(amount: int):
 
         try:
 
+            # Setup Session
+            sess = requests.Session()
+            sess.headers.update(config['request_data']['headers'])
+
             # Generate URL & Get page
             url = url_template + generate_random_string()
-            response = requests.get(url, headers=config['request_data']['headers'])
+            response = sess.get(url)
 
             # If page is found
             if response.status_code == 200:
 
                 parser = BeautifulSoup(response.text, "html.parser")
-                image_url = parser.find('meta', attrs= {'property': 'og:image'}).get('content')
+                image_url = parser.find('meta', attrs= {'property': 'og:image'})
+
+                if not image_url:
+                    print(f"[ {Fore.RED}FAIL{Fore.RESET} ] Cloudflare blocked request")
+
+                image_url = image_url.get('content')
 
                 # If image is nothing
-                if image_url != '//st.prntscr.com/2023/07/24/0635/img/0_173a7b_211be8ff.png':
+                if 'http' not in image_url:
+                    print(f"[ {Fore.RED}FAIL{Fore.RESET} ] Screenshot was removed")
+                    continue
 
-                    # Save found screenshot
-                    with open('screenshots/index.png', 'w', encoding='utf8') as file:
-                        file.write(response.text)
+                # Get image & Generate name
+                image = sess.get(image_url)
+                image_name = generate_random_string(10)
+                
+                with open(os.path.join('screenshots', image_name + image_url[-5:]), 'wb') as file:
+                    file.write(image.content)
+
+                print(f"[ {Fore.GREEN}SUCCESS{Fore.RESET} ] Image is found!")
+
 
             # If page is not found
             elif response.status_code == 404:
-                print('404 Error')
+                print(f'[ {Fore.RED}FAIL{Fore.RESET} ] Page is not Found')
+                continue
         
         # Throw an error
         except Exception as e:
-            print('Error Happened:', e)
-
-
-def generate_random_string(length=7):
-    letters = "abcdefghijklmnopqrstuvwxyz0123456789"
-    return ''.join(random.choice(letters) for _ in range(length))
+            print(f'[ {Fore.RED}EXCEPTION {Fore.RESET}]', e)
 
 
